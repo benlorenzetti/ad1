e15a8a3e801c5ae4a5d2b49adee0c10dc4826a53
Ben Lorenzetti <benlorenzetti@unknownB6750E17D412.attlocal.net>
2018-02-04 15:59:10 -0500

    merging from laptop

--- impl/integers.h
+++ impl/integers.h
@@ -4,7 +4,9 @@ using namespace std;
 
 
 class W_m;
-class W_2m;
+class W_mxm;
+class N_m;
+class N_mxm;
 
 char* str(W_m);  
 char* left_right_fix(char*);
@@ -12,51 +14,115 @@ char* left_right_fix(char*);
 class W_m {
     public:
     unsigned char datum;
+    W_m();
+    W_m(unsigned int); 
     W_m operator+(W_m);
     W_m operator-(W_m);
-    W_m operator*(W_m);
-    W_m operator/(W_m);
+    W_mxm operator*(W_m);
     W_m operator%(W_m);
     W_m operator<<(unsigned char);
+    W_m operator>>(unsigned char);
+    W_m operator~();
+    bool operator==(W_m);
 };
 
-W_m upperhalf(W_m);
-W_m lowerhalf(W_m);
+W_m uhalf(W_m);
+W_m lhalf(W_m);
+W_m uhalf(W_mxm);
+W_m lhalf(W_mxm);
+N_m uhalf(N_mxm);
+N_m lhalf(N_mxm);
+W_m toW_m(N_m);
 
 class W_mxm {
     public:
     W_m datum[2];
-    W_mxm(W_m, W_m);
 };
 
-W_m upperhalf(W_mxm);
-W_m lowerhalf(W_mxm);
+class N_m {
+    public:
+    W_m datum;
+    N_m(W_m);
+    N_m(unsigned int);
+    N_m operator+(N_m);
+    N_m operator-(N_m);
+    N_mxm operator*(N_m);
+};
 
-W_m upperhalf(W_mxm w) {
+class N_mxm {
+    public:
+    N_m datum[2];  
+};
+
+N_m::N_m(W_m w) {
+    datum = w;
+}
+
+N_m N_m::operator+(N_m n2) {
+    N_m result = {datum};
+    result.datum = result.datum + n2.datum;
+    return result;
+}
+
+N_m N_m::operator-(N_m n2) {
+    N_m result = {datum};
+    result.datum = result.datum - n2.datum;
+    return result;
+}
+
+N_m uhalf(N_mxm n) {
+    N_m result(n.datum[1]);
+    return result;
+}
+
+N_m lhalf(N_mxm n) {
+    N_m result(n.datum[0]);
+    return result;
+}
+
+W_m toW_m(N_m n) {
+    return ~n.datum + W_m(1);
+}
+
+W_m uhalf(W_mxm w) {
     return w.datum[1];
 }
 
-W_m lowerhalf(W_mxm w) {
+W_m lhalf(W_mxm w) {
     return w.datum[0];
 }
 
-W_mxm::W_mxm(W_m base, W_m scalar) {
+W_mxm W_m::operator*(W_m scalar) {
     W_m w1, w2, w3, w4, upper, inner, lower;
-    w1 = upperhalf(base);
-    w2 = lowerhalf(base);
-    w3 = upperhalf(scalar);
-    w4 = lowerhalf(scalar);
-    upper = w1 * w3;
-    inner = w1*w4 + w2*w3;
-    lower = w2 * w4;
-    datum[1] = upper + upperhalf(inner);
-    datum[0] = lower + (lowerhalf(inner) << (sizeof(W_m) << 2));
+    W_mxm result;
+    w1 = uhalf(*this);
+    w2 = lhalf(*this);
+    w3 = uhalf(scalar);
+    w4 = lhalf(scalar);
+    upper.datum = w1.datum * w3.datum;
+    inner.datum = w1.datum*w4.datum + w2.datum*w3.datum;
+    lower.datum = w2.datum * w4.datum;
+    result.datum[1] = upper + uhalf(inner);
+    result.datum[0] = lower + (lhalf(inner) << (sizeof(W_m) << 2));
+    return result;
 }
 
-W_m W_m::operator*(W_m w2) {
+W_m::W_m() {
+    datum = 0;
+}
+
+W_m::W_m(unsigned int w) {
+    datum = w;
+}
+
+W_m W_m::operator~() {
     W_m result = {datum};
-    result.datum *= w2.datum;
+    result.datum = ~result.datum;
     return result;
+}
+
+bool W_m::operator==(W_m w2) {
+    return (datum == w2.datum);
 }
 
 W_m W_m::operator/(W_m w2) {
@@ -77,6 +143,12 @@ W_m W_m:: operator<<(unsigned char dist) {
     return result;
 }
 
+W_m W_m:: operator>>(unsigned char dist) {
+    W_m result = {datum};
+    result.datum <<= dist;
+    return result;
+}
+
 W_m W_m::operator+(W_m w2) {
     W_m result;
     result.datum = datum + w2.datum;
@@ -89,12 +161,12 @@ W_m W_m::operator-(W_m w2) {
     return result;
 }
 
-W_m upperhalf(W_m w) {
+W_m uhalf(W_m w) {
     w.datum >>= (sizeof(W_m) << 2);
     return w;
 }
 
-W_m lowerhalf(W_m w) {
+W_m lhalf(W_m w) {
     uintptr_t MASK = 1;
     MASK <<= sizeof(W_m) << 2;
     MASK -= 1;
